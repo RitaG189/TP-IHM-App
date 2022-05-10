509607b38b254686011eb655605806f91fdbc8dc
Rita Guimarães <kikasgui@gmail.com>
2022-05-10 19:46:48 +0100

    -

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'TP',
+  appId: 'pt.ihm.trabalho',
+  appName: 'TugaKidsClothes',
   webDir: 'www',
   bundledWebRuntime: false
 };
